fea3b1e9aa313a459e8b301f4a95b07b476f039f
sonnygauran <sonnygauran@gmail.com>
2010-03-23 14:06:24 +0800

    Fresh import of cake bake. Configured core.php to Cache.disable = true

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'chieftain',
+		'password' => 'cgmms',
+		'database' => 'chieftain',
+		'prefix' => 'chieftain_',
+	);
+}
+?>
